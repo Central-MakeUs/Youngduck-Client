a2f4c9f5cdf4bdae84cd8190b46cff2fac85df65
gs0428 <lider28@naver.com>
2024-01-05 22:08:32 +0900

    chore: caption text style 설정

---
+++ src/styles/texts/caption.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const caption = StyleSheet.create({
+  Caption: {
+    fontWeight: '400',
+    fontSize: 10,
+    lineHeight: 14,
+  },
+});
+
+export default caption;
