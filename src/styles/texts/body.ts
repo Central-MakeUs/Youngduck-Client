2d4f33ba0a523e23508a4e522caf3644fd42fd31
gs0428 <lider28@naver.com>
2024-01-05 22:00:17 +0900

    chore: body text style 설정

---
+++ src/styles/texts/body.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const body = StyleSheet.create({
+  Body1: {
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  Body2: {
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 20,
+  },
+});
+
+export default body;
