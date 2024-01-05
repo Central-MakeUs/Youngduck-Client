977fc41e1c57c8c3f0242079bc3f93dd8283b260
gs0428 <lider28@naver.com>
2024-01-05 22:08:43 +0900

    chore: chips text style 설정

---
+++ src/styles/texts/chips.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const chips = StyleSheet.create({
+  Chips1: {
+    fontWeight: '400',
+    fontSize: 12,
+    lineHeight: 18,
+  },
+  Chips2: {
+    fontWeight: '400',
+    fontSize: 11,
+    lineHeight: 16,
+  },
+});
+
+export default chips;
