61a37d642d5038ddc97c92e6aa55116b763d3168
gs0428 <lider28@naver.com>
2024-01-05 22:09:05 +0900

    chore: title text style 설정

---
+++ src/styles/texts/title.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const title = StyleSheet.create({
+  Title1: {
+    fontWeight: '700',
+    fontSize: 24,
+    lineHeight: 30,
+  },
+  Title2: {
+    fontWeight: '700',
+    fontSize: 20,
+    lineHeight: 28,
+  },
+});
+
+export default title;
