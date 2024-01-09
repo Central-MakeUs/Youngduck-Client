ad9e749283c40a2a1acfaed7b3c9f81816a8e64d
gs0428 <lider28@naver.com>
2024-01-09 14:22:51 +0900

    feat: 로그인 함수 구현 (현재 키 X)

--- src/components/auth/AppleLogin.tsx
+++ src/components/auth/AppleLogin.tsx
@@ -2,6 +2,25 @@ import appleAuth, {
   AppleButton,
 } from '@invertase/react-native-apple-authentication';
 
+async function handleSignInApple() {
+  // performs login request
+  const appleAuthRequestResponse = await appleAuth.performRequest({
+    requestedOperation: appleAuth.Operation.LOGIN,
+    // Note: it appears putting FULL_NAME first is important, see issue #293
+    requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+  });
+  // get current authentication state for user
+  // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+  const credentialState = await appleAuth.getCredentialStateForUser(
+    appleAuthRequestResponse.user,
+  );
+
+  // use credentialState response to ensure the user is authenticated
+  if (credentialState === appleAuth.State.AUTHORIZED) {
+    // user is authenticated
+  }
+}
+
 function AppleLogin() {
   return (
     <AppleButton
@@ -11,7 +30,7 @@ function AppleLogin() {
         width: '100%', // You must specify a width
         height: 45, // You must specify a height
       }}
-      onPress={() => {}}
+      onPress={handleSignInApple}
     />
   );
 }
