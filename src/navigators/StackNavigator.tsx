af6a34510d5cafd61627802fee59101da4f93c20
suminhan123 <suminh324@gmail.com>
2024-01-11 15:15:12 +0900

    style: bottomTab svg 생성

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -1,13 +1,14 @@
 import stackScreens from '@/constants/stackScreens';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import BottomTabNavigator from './BottomTabNavigator';
+
 import LoginScreen from '@/screens/login/LoginScreen';
+import BottomTabNavigator from './bottomTabNavigator';
 
 function StackNavigator() {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name={stackScreens.LoginScreen} component={LoginScreen} />
+      {/*<Stack.Screen name={stackScreens.LoginScreen} component={LoginScreen} />*/}
       <Stack.Screen
         name={stackScreens.BottomTabScreens}
         component={BottomTabNavigator}
