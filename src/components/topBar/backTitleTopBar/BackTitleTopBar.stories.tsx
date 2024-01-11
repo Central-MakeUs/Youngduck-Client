9a6a43be2ce213cacc7ba80a7674a502ba1f1a67
suminhan123 <suminh324@gmail.com>
2024-01-12 00:32:06 +0900

    comment: backTitle 컴포넌트 주석 제거

--- src/components/topBar/backTitleTopBar/BackTitleTopBar.stories.tsx
+++ src/components/topBar/backTitleTopBar/BackTitleTopBar.stories.tsx
@@ -1,15 +1,15 @@
-//import {ComponentMeta, ComponentStory} from '@storybook/react';
-//import BackTitleTopBar from '.';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import BackTitleTopBar from '.';
 
-//export default {
-//  title: 'components/TopBar/BackTitleTopBar',
-//  component: BackTitleTopBar,
-//} as ComponentMeta<typeof BackTitleTopBar>;
+export default {
+  title: 'components/TopBar/BackTitleTopBar',
+  component: BackTitleTopBar,
+} as ComponentMeta<typeof BackTitleTopBar>;
 
-//export const Basic: ComponentStory<typeof BackTitleTopBar> = args => (
-//  <BackTitleTopBar {...args} />
-//);
+export const Basic: ComponentStory<typeof BackTitleTopBar> = args => (
+  <BackTitleTopBar {...args} />
+);
 
-//Basic.args = {
-//  text: '제목',
-//};
+Basic.args = {
+  text: '제목',
+};
