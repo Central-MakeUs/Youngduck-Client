34da5284fba9c09180c60f44e6c6e4b780012681
suminhan123 <suminh324@gmail.com>
2024-01-12 20:54:58 +0900

    design: 이번주 스크리닝 ui 작업

--- src/screens/screening/ScreeningScreen.tsx
+++ src/screens/screening/ScreeningScreen.tsx
@@ -1,11 +1,51 @@
 import SubTitleTopBar from '@/components/topBar/subTitleTopBar';
-import {View} from 'react-native';
+import {FlatList, View} from 'react-native';
+import WeeklyScreening from './components/weeklyScreening';
 
 function ScreeningScreen() {
+  // 이번주 스크리닝 데이터
+  const data = [
+    {
+      id: 1,
+      title: 'Dromapic 상영회',
+      date: '2024.01.05',
+      type: '상영전',
+      remain: 'D-2',
+    },
+    {
+      id: 2,
+      title: 'Dromapic 상영회',
+      date: '2024.01.05',
+      type: '상영전',
+      remain: 'D-2',
+    },
+    {
+      id: 3,
+      title: 'Dromapic 상영회',
+      date: '2024.01.05',
+      type: '상영전',
+      remain: 'D-2',
+    },
+    {
+      id: 4,
+      title: 'Dromapic 상영회',
+      date: '2024.01.05',
+      type: '상영전',
+      remain: 'D-2',
+    },
+  ];
+  const renderItem = () => <WeeklyScreening />;
   return (
     <View>
       {/*이미지 자리*/}
-      <SubTitleTopBar text="이번주 스크리닝" mt={12} />
+      <SubTitleTopBar text="이번주 스크리닝" mt={12} mb={8} />
+      <FlatList
+        horizontal
+        data={data}
+        renderItem={renderItem}
+        contentContainerStyle={{paddingHorizontal: 20}}
+      />
+      <SubTitleTopBar text="관객 리뷰" mt={24} />
     </View>
   );
 }
