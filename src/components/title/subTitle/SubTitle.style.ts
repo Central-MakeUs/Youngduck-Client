433cffe941d8e075abc7166f45fe3dd97d0b349c
gs0428 <lider28@naver.com>
2024-01-13 16:29:29 +0900

    chore: subtitle style 수정

--- src/components/title/subTitle/SubTitle.style.ts
+++ src/components/title/subTitle/SubTitle.style.ts
@@ -2,10 +2,6 @@ import {StyleSheet} from 'react-native';
 
 export const subTitleStyles = StyleSheet.create({
   container: {
-    paddingHorizontal: 16,
     paddingVertical: 8,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    width: '100%',
   },
 });
