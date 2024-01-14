5910d443ef2099f17188d568afa883df97d709f0
gs0428 <lider28@naver.com>
2024-01-15 02:19:38 +0900

    chore: stackNavigation 타입 수정

--- src/hooks/useNavigator.ts
+++ src/hooks/useNavigator.ts
@@ -1,8 +1,10 @@
 import {BottomTabParamList, RootStackParamList} from '@/types/navigator';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const useNavigator = () => {
-  const stackNavigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const stackNavigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   const tabNavigation = useNavigation<NavigationProp<BottomTabParamList>>();
 
   return {stackNavigation, tabNavigation};
