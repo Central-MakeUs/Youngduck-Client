0ca0545c07f92ace7e592803479af38c269ef6a6
gs0428 <lider28@naver.com>
2024-01-15 00:33:03 +0900

    feat: 약관동의 전역변수 설정

---
+++ src/stores/agreeTermStore.ts
@@ -0,0 +1,18 @@
+import {create} from 'zustand';
+
+interface StoreState {
+  allAgree: boolean;
+  isFinishAgree: boolean;
+}
+
+interface StoreAction {
+  setAllAgree: (allAgree: boolean) => void;
+  setIsFinishAgree: (isFinishAgree: boolean) => void;
+}
+
+export const agreeTermStore = create<StoreState & StoreAction>(set => ({
+  allAgree: false,
+  isFinishAgree: false,
+  setAllAgree: (state: boolean) => set({allAgree: state}),
+  setIsFinishAgree: (state: boolean) => set({isFinishAgree: state}),
+}));
