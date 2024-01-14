4401b31b8f5b7c66dae326676e11b3d26493c185
suminhan123 <suminh324@gmail.com>
2024-01-14 20:19:21 +0900

    design: 등록하기 페이지 퍼블리싱

--- src/screens/screening/writing/WritingScreen.tsx
+++ src/screens/screening/writing/WritingScreen.tsx
@@ -1,10 +1,49 @@
-import {Text, View} from 'react-native';
+import DefaultContainer from '@/components/container/defaultContainer';
+import Typography from '@/components/typography';
+import palette from '@/styles/theme/color';
+import ScreeningGallery from './components/screeningGallery';
+import TextInput from '@/components/textInput';
+import {useCallback, useState} from 'react';
 
 const WritingScreen = () => {
+  const [title, setTitle] = useState<string>('');
+  const [group, setGroup] = useState<string>('');
+
+  const onChangeTitle = useCallback((text: string) => {
+    setTitle(text);
+  }, []);
+
+  const onChangeGroup = useCallback((group: string) => {
+    setGroup(group);
+  }, []);
   return (
-    <View>
-      <Text>스크리닝 작성 페이지</Text>
-    </View>
+    <DefaultContainer>
+      <Typography mt={40} mb={20} style="Title2" color={palette.Another.Black}>
+        상영 정보를 입력해주세요!
+      </Typography>
+      {/*갤러리 선택*/}
+      <ScreeningGallery />
+
+      {/*타이틀*/}
+      <TextInput
+        value={title}
+        placeholder="입력해주세용"
+        onChangeInput={onChangeTitle}
+        maxLength={10}
+        title="타이틀"
+        content="타이틀을 입력해주세요"
+      />
+
+      {/*주최명*/}
+      <TextInput
+        value={group}
+        placeholder="입력해주세용"
+        onChangeInput={onChangeGroup}
+        maxLength={10}
+        title="주최명"
+        content="주최명을 입력해주세요"
+      />
+    </DefaultContainer>
   );
 };
 export default WritingScreen;
