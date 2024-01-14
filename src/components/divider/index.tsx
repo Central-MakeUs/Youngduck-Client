a50f8934a18b88fa13d8eefcc9746ee7e90ddec5
gs0428 <lider28@naver.com>
2024-01-15 00:27:26 +0900

    chore: divider 컴포넌트 수정

--- src/components/divider/index.tsx
+++ src/components/divider/index.tsx
@@ -1,7 +1,16 @@
 import {View} from 'react-native';
 import {dividerStyle} from './Divider.style';
+import {CommonMarginVerticalProps} from '@/types/ui';
 
-const Divider = () => {
-  return <View style={dividerStyle.container} />;
+const Divider = ({mt, mb}: CommonMarginVerticalProps) => {
+  return (
+    <View
+      style={{
+        ...dividerStyle.container,
+        marginTop: mt ? mt : undefined,
+        marginBottom: mb ? mb : undefined,
+      }}
+    />
+  );
 };
 export default Divider;
