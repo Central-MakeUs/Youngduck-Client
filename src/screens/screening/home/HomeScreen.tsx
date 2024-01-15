a2f3b24c2b5e72ab0b2c0112ffefe0f54a37aad8
suminhan123 <suminh324@gmail.com>
2024-01-15 19:06:08 +0900

    design: content margin 수정

--- src/screens/screening/home/HomeScreen.tsx
+++ src/screens/screening/home/HomeScreen.tsx
@@ -61,19 +61,22 @@ function HomeScreen() {
     <DefaultScrollContainer>
       {/*이미지 자리*/}
       <Banner type="screening" onPress={handleGoWriting} />
-      <SubTitle text="이번주 스크리닝" mt={12} mb={8} />
-      <FlatList
-        horizontal
-        data={data}
-        renderItem={renderItem}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{paddingHorizontal: 16}}
-      />
-      <SubTitle text="관객 리뷰" mt={24} mb={8} />
-      <ReviewScreening />
-      <Divider />
-      <SubTitle text="실시간 새 소식" mt={16} mb={8} />
       <DefaultContainer>
+        <SubTitle text="이번주 스크리닝" mt={12} mb={8} />
+        <FlatList
+          horizontal
+          data={data}
+          renderItem={renderItem}
+          showsHorizontalScrollIndicator={false}
+        />
+        <SubTitle text="관객 리뷰" mt={24} mb={8} />
+      </DefaultContainer>
+      <ReviewScreening />
+
+      <Divider />
+
+      <DefaultContainer>
+        <SubTitle text="실시간 새 소식" mt={16} mb={8} />
         <RecentScreening />
         <RecentScreening />
         <RecentScreening />
