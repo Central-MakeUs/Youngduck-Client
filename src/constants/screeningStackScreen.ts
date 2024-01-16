83a0ea05693b564308ee5469fd431cda0652835b
suminhan123 <suminh324@gmail.com>
2024-01-16 23:11:44 +0900

    feat: screeningStackScreen enum 변수 수정

--- src/constants/screeningStackScreen.ts
+++ src/constants/screeningStackScreen.ts
@@ -1,8 +1,5 @@
 const enum ScreeningStackScreen {
   HomeScreen = 'HomeScreen',
-  WritingScreen = 'WritingScreen',
-  DetailScreen = 'DetailScreen',
-  ReviewWritingScreen = 'ReviewWritingScreen',
   ScreeningListScreen = 'ScreeningListScreen',
 }
 
