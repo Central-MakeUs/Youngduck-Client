50d590d85bdc1ecd2050177e0cee0262954295bf
suminhan123 <suminh324@gmail.com>
2024-01-17 11:57:15 +0900

    design: 공통 input 스타일 생성

--- src/styles/Input.style.ts
+++ src/styles/Input.style.ts
@@ -1,5 +1,6 @@
 import {ITextInput, TextInputStyle} from '@/types/ui';
 import palette from './theme/color';
+import {StyleSheet} from 'react-native';
 
 // 공통 input styles
 export const inputTypes: Record<ITextInput, TextInputStyle> = {
@@ -23,4 +24,28 @@ export const inputTypes: Record<ITextInput, TextInputStyle> = {
     titleColor: palette.Text.Strong,
     contentColor: palette.Text.Alternative,
   },
+  warning: {
+    borderColor: palette.State.Point,
+    titleColor: palette.State.Point,
+    contentColor: palette.State.Point,
+  },
 };
+
+export const inputStyles = StyleSheet.create({
+  input: {
+    width: '100%',
+    height: 40,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  button: {
+    position: 'relative',
+    justifyContent: 'center',
+  },
+  logo: {
+    position: 'absolute',
+    right: 16,
+  },
+});
