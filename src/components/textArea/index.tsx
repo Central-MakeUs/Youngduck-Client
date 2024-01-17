a9ec3b05906acb4db9463bf6d520b55828b7786d
suminhan123 <suminh324@gmail.com>
2024-01-17 13:30:06 +0900

    feat: textArea 컴포넌트 생성

---
+++ src/components/textArea/index.tsx
@@ -0,0 +1,35 @@
+import {View, TextInput} from 'react-native';
+import Typography from '../typography';
+import palette from '@/styles/theme/color';
+
+interface ITextAreaProps {
+  value: string;
+  onChange: (value: string) => void;
+  title: string;
+  maxLength: number;
+  placeholder: string;
+  height: number;
+}
+const TextArea = ({
+  value,
+  onChange,
+  title,
+  maxLength,
+  placeholder,
+  height,
+}: ITextAreaProps) => {
+  return (
+    <View>
+      <Typography color={palette.Text.Strong} style="Label2">
+        {title}
+      </Typography>
+      <TextInput
+        style={{height: height}}
+        value={value}
+        placeholder="이름을 입력해주세요."
+        multiline={true}
+      />
+    </View>
+  );
+};
+export default TextArea;
