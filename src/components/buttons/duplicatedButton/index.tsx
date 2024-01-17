e5f4ee2d30ae3c0490d8d217b61628500f6f28f6
suminhan123 <suminh324@gmail.com>
2024-01-17 23:28:49 +0900

    refactor: Input 중복 확인 버튼 생성

---
+++ src/components/buttons/duplicatedButton/index.tsx
@@ -0,0 +1,30 @@
+import {Pressable} from 'react-native';
+import {duplicatedStyles} from './DuplicatedButton.style';
+import Typography from '@/components/typography';
+import palette from '@/styles/theme/color';
+
+interface IDuplicatedProps {
+  value: string;
+  isDuplicated: boolean;
+  onPress: () => void;
+}
+const DuplicatedButton = ({value, isDuplicated, onPress}: IDuplicatedProps) => {
+  <Pressable
+    style={
+      value.length >= 2 && isDuplicated
+        ? duplicatedStyles.activated
+        : duplicatedStyles.deActivated
+    }
+    onPress={onPress}>
+    <Typography
+      style="Chips2"
+      color={
+        value.length >= 2 && isDuplicated
+          ? palette.Primary.Deep
+          : palette.Text.Alternative
+      }>
+      중복확인
+    </Typography>
+  </Pressable>;
+};
+export default DuplicatedButton;
