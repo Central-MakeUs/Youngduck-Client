9a3f1c1ecc459d8c7ed697c12fa9bfd3b94a117d
gs0428 <lider28@naver.com>
2024-01-17 16:28:58 +0900

    feat: 팝콘작 추천 목록 스크린 퍼블리싱

--- src/screens/popCornParty/recommandList/RecommandListScreen.tsx
+++ src/screens/popCornParty/recommandList/RecommandListScreen.tsx
@@ -1,7 +1,56 @@
-import {Text} from 'react-native';
+import DefaultContainer from '@/components/container/defaultContainer';
+import PopcornItem from '@/components/items/popcornItem';
+import SubTitleDescription from '@/components/title/subTitleDescription';
+import {IPopcornItemProps, IRenderItemProps} from '@/types/popcornParty';
+import {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList} from 'react-native';
+import {defaultDatas, moreDats} from './dummy';
 
 function RecommandListScreen() {
-  return <Text>RecommandListScreen</Text>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [popcornDatas, setPopcornDatas] =
+    useState<IPopcornItemProps[]>(defaultDatas);
+
+  const renderItem = ({item}: IRenderItemProps<IPopcornItemProps>) => (
+    <PopcornItem
+      key={item.title}
+      id={item.id}
+      imageURL={item.imageURL}
+      title={item.title}
+      count={item.count}
+      nickname={item.nickname}
+      content={item.content}
+      isVoted={item.isVoted}
+    />
+  );
+
+  const renderMoreItems = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setPopcornDatas([...popcornDatas, ...moreDats]);
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+  return (
+    <DefaultContainer>
+      <SubTitleDescription
+        text="다음 주 팝콘작 투표"
+        subTitle={`1월 7일 ~ 1월 13일의 팝콘작을 선정해 주세요.\n상위 3편의 영화가 다음 주 오픈됩니다.`}
+        mb={8}
+      />
+      <FlatList
+        data={popcornDatas}
+        renderItem={renderItem}
+        keyExtractor={(item: IPopcornItemProps) => item.id.toString()}
+        onEndReached={renderMoreItems}
+      />
+      {isLoading && <ActivityIndicator style={{marginBottom: 10}} />}
+    </DefaultContainer>
+  );
 }
 
 export default RecommandListScreen;
