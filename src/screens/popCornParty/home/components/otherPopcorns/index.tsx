df2b1aa34f2c2ff558fc0dfe7314cefaad499fa5
gs0428 <lider28@naver.com>
2024-01-17 14:52:34 +0900

    feat: 팝콘작 투표로 이동 기능 구현

--- src/screens/popCornParty/home/components/otherPopcorns/index.tsx
+++ src/screens/popCornParty/home/components/otherPopcorns/index.tsx
@@ -1,10 +1,21 @@
 import BoxButton from '@/components/buttons/boxButton';
 import DefaultContainer from '@/components/container/defaultContainer';
+import stackScreens from '@/constants/stackScreens';
+import useNavigator from '@/hooks/useNavigator';
 
 const OtherPopcorns = () => {
+  const {stackNavigation} = useNavigator();
+
+  const goToRecommandListScreen = () =>
+    stackNavigation.navigate(stackScreens.RecommandListScreen);
+
   return (
     <DefaultContainer>
-      <BoxButton onPress={() => {}} variant="default" mt={12} mb={8}>
+      <BoxButton
+        onPress={goToRecommandListScreen}
+        variant="default"
+        mt={12}
+        mb={8}>
         5건의 투표작 모두 보기
       </BoxButton>
       <BoxButton onPress={() => {}} mb={32}>
