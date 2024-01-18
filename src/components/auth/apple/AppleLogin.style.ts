4b7d0515030375e9a5cf0e9a8fef53334521d162
GwangSoo <114225974+gs0428@users.noreply.github.com>
2024-01-18 16:01:57 +0900

    [FEAT] 애플 로그인 (#35)
    * chore: apple login 라이브러리 설치
    
    * design: apple login 버튼 구현
    
    * feat: 로그인 함수 구현 (현재 키 X)
    
    * chore: apple login 라이브러리 설치
    
    * design: apple login 버튼 구현
    
    * feat: 로그인 함수 구현 (현재 키 X)
    
    * feat: 애플 로그인 기능 1차 구현
    
    * chore: apple logo svg 추가
    
    * feat: 애플 로그인 구현 완료
    
    * feat: 카카오 로그인 컴포넌트 auth 폴더로 이동
    
    * feat: platform이 ios일 때만 버튼 보이도록 설정

---
+++ src/components/auth/apple/AppleLogin.style.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const appleLoginStyle = StyleSheet.create({
+  button: {
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 12,
+    flexDirection: 'row',
+    borderRadius: 8,
+  },
+});
+
+export default appleLoginStyle;
--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,37 +1,17 @@
-import {getKakaoProfile} from '@/apis/social';
-import {postLoginUser} from '@/apis/user';
-import * as Kakao from '@react-native-seoul/kakao-login';
-
 import LoginContainer from '@/components/container/loginContainer';
-import KakaoLogin from '@/components/buttons/kakaoLogin';
+import KakaoLogin from '@/components/auth/kakao';
 
-import {Image, Pressable, Text, View} from 'react-native';
+import {Image, Platform, Pressable, Text, View} from 'react-native';
 import loginScreenStyles from '@/screens/login/LoginScreen.style';
 import {defaultImages} from '@/assets';
 import useNavigator from '@/hooks/useNavigator';
 import stackScreens from '@/constants/stackScreens';
+import AppleLogin from '@/components/auth/apple';
 
 function LoginScreen() {
   const {stackNavigation} = useNavigator();
-  // 카카오 로그인 함수
-  const handleSignInKakao = async (): Promise<void> => {
-    Kakao.login()
-      .then(async result => {
-        const idToken = result.idToken;
-        const res = postLoginUser('KAKAO', idToken);
-        console.log('로그인 응답', res);
-        getKakaoProfile();
-      })
-      .catch(error => {
-        if (error.code === 'E_CANCELLED_OPERATION') {
-          console.log('Login Cancel', error.message);
-        } else {
-          console.log(`Login Fail(code:${error.code})`, error.message);
-        }
-      });
-    // 일단 로그인 후 홈으로 이동
-    stackNavigation.navigate('BottomTabScreens');
-  };
+  const platform = Platform.OS;
+
   return (
     <LoginContainer>
       <View style={loginScreenStyles.wrapper}>
@@ -41,8 +21,8 @@ function LoginScreen() {
         <Image source={defaultImages.popCornMate} />
       </View>
       <View style={loginScreenStyles.wrapper}>
-        <KakaoLogin onPress={handleSignInKakao} />
-        <KakaoLogin onPress={() => {}} />
+        <KakaoLogin />
+        {platform === 'ios' && <AppleLogin />}
       </View>
       <Pressable
         onPress={() => stackNavigation.navigate(stackScreens.SignupScreen)}>
