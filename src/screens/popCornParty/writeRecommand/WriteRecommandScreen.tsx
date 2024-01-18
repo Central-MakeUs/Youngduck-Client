9908af44d7ebc87f581f1f689c773b5106bfcff3
gs0428 <lider28@naver.com>
2024-01-18 16:24:19 +0900

    feat: subtitle 구현

--- src/screens/popCornParty/writeRecommand/WriteRecommandScreen.tsx
+++ src/screens/popCornParty/writeRecommand/WriteRecommandScreen.tsx
@@ -1,7 +1,17 @@
-import {Text} from 'react-native';
+import DefaultContainer from '@/components/container/defaultContainer';
+import SubTitleDescription from '@/components/title/subTitleDescription';
+import useGetVoteDateRange from '@/hooks/useGetVoteDateRange';
 
 function WriteRecommandScreen() {
-  return <Text>WriteRecommandScreen</Text>;
+  const {startDate, endDate} = useGetVoteDateRange();
+  return (
+    <DefaultContainer>
+      <SubTitleDescription
+        text="다음 주의 팝콘작을 추천해 주세요"
+        subTitle={`추천하신 영화는 ${startDate}부터\n${endDate}의 팝콘작 후보로 게시됩니다.`}
+      />
+    </DefaultContainer>
+  );
 }
 
 export default WriteRecommandScreen;
