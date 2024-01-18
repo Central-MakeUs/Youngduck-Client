13469016e5917855013442799aa2500ef3b27eb4
gs0428 <lider28@naver.com>
2024-01-18 21:02:31 +0900

    feat: 추천 이유 input 구현

--- src/screens/popCornParty/writeRecommand/WriteRecommandScreen.tsx
+++ src/screens/popCornParty/writeRecommand/WriteRecommandScreen.tsx
@@ -1,11 +1,16 @@
 import DefaultContainer from '@/components/container/defaultContainer';
 import ButtonInput from '@/components/inputs/buttonInput';
+import TextArea from '@/components/inputs/textArea';
 import SubTitleDescription from '@/components/title/subTitleDescription';
 import useGetVoteDateRange from '@/hooks/useGetVoteDateRange';
+import {useState} from 'react';
 import {View} from 'react-native';
 
 function WriteRecommandScreen() {
   const {startDate, endDate} = useGetVoteDateRange();
+  const [reason, setReason] = useState<string>('');
+
+  const inputReason = (e: string) => setReason(e);
   return (
     <DefaultContainer>
       <SubTitleDescription
@@ -14,12 +19,23 @@ function WriteRecommandScreen() {
         mt={24}
         mb={32}
       />
-      <ButtonInput
-        value={''}
-        placeholder="클릭하면 영화를 검색할 수 있어요"
-        title="추천 영화"
-        category="search"
-        setValue={() => {}}
+      <View style={{marginBottom: 24}}>
+        <ButtonInput
+          value={''}
+          placeholder="클릭하면 영화를 검색할 수 있어요"
+          title="추천 영화"
+          category="search"
+          setValue={() => {}}
+          essential
+        />
+      </View>
+      <TextArea
+        value={reason}
+        onChangeInput={inputReason}
+        maxLength={100}
+        placeholder={'최소 10자에서 최대 100자까지 입력할 수 있어요'}
+        height={144}
+        title="추천하는 이유"
         essential
       />
     </DefaultContainer>
