8c9fa7a30814bf8956d068ad10e5ef336c4d6dee
gs0428 <lider28@naver.com>
2024-01-19 04:02:39 +0900

    feat: search bottom sheet 구현

--- src/screens/popCornParty/writeRecommand/components/searchBottomSheet/SearchBottomSheet.style.ts
+++ src/screens/popCornParty/writeRecommand/components/searchBottomSheet/SearchBottomSheet.style.ts
@@ -1,12 +1,23 @@
 import {StyleSheet} from 'react-native';
+import {getScreenSize} from '@/utils/getScreenSize';
 
-const searchBottomSheetStyles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 16,
-  },
-});
+const {screenHeight} = getScreenSize();
+
+interface ISearchBottomSheetStylesProp {
+  bottom: number;
+}
+
+const searchBottomSheetStyles = ({bottom}: ISearchBottomSheetStylesProp) =>
+  StyleSheet.create({
+    container: {flex: 1, paddingBottom: screenHeight / 3 + bottom + 16},
+    wrap: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 16,
+    },
+    totalResultWrap: {flexDirection: 'row', marginTop: 16},
+    selectButton: {flex: 1, flexDirection: 'column-reverse'},
+  });
 
 export default searchBottomSheetStyles;
