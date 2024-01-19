e82a518015b33a7b805d2d4df38771574d8c2724
suminhan123 <suminh324@gmail.com>
2024-01-19 19:54:30 +0900

    design: tab 디자인 추가

--- src/screens/screening/detail/DetailScreen.tsx
+++ src/screens/screening/detail/DetailScreen.tsx
@@ -1,14 +1,38 @@
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import DetailInfoScreen from './tabs/DetailInfoScreen';
 import DetailReviewScreen from './tabs/DetailReviewScreen';
+import palette from '@/styles/theme/color';
+import {tabBarLabel} from '@/constants/tabScreens';
 
 const Tab = createMaterialTopTabNavigator();
 
 const DetailScreen = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="First" component={DetailInfoScreen} />
-      <Tab.Screen name="Second" component={DetailReviewScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: palette.Text.Normal,
+        tabBarInactiveTintColor: palette.Text.Assistive,
+        tabBarStyle: {backgroundColor: palette.Another.White},
+        tabBarLabelStyle: {
+          fontSize: 16,
+          fontFamily: 'Pretendard',
+          fontWeight: '600',
+          lineHeight: 24,
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: palette.Text.Normal,
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }}>
+      <Tab.Screen
+        name={tabBarLabel.DetailInfoScreen}
+        component={DetailInfoScreen}
+      />
+      <Tab.Screen
+        name={tabBarLabel.DetailReviewScreen}
+        component={DetailReviewScreen}
+      />
     </Tab.Navigator>
   );
 };
