5a6f33a8f694bc0546d8642ccfd5e1576a2ff83f
suminhan123 <suminh324@gmail.com>
2024-01-19 23:40:08 +0900

    design: 스크리닝 상세 정보 컨텐츠 퍼블리싱

--- src/screens/screening/detail/DetailScreen.tsx
+++ src/screens/screening/detail/DetailScreen.tsx
@@ -3,37 +3,51 @@ import DetailInfoScreen from './tabs/DetailInfoScreen';
 import DetailReviewScreen from './tabs/DetailReviewScreen';
 import palette from '@/styles/theme/color';
 import {tabBarLabel} from '@/constants/tabScreens';
+import {View} from 'react-native';
+import {ScreenRouteProp} from '@/types/navigator';
+import DetailBottomButton from '@/components/bottomButtons/detailBottomButton';
 
 const Tab = createMaterialTopTabNavigator();
 
-const DetailScreen = () => {
+type DetailScreenProps = {
+  route: ScreenRouteProp<'DetailScreen'>;
+};
+
+const DetailScreen = ({route}: DetailScreenProps) => {
+  const {id} = route.params;
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: palette.Text.Normal,
-        tabBarInactiveTintColor: palette.Text.Assistive,
-        tabBarStyle: {backgroundColor: palette.Another.White},
-        tabBarLabelStyle: {
-          fontSize: 16,
-          fontFamily: 'Pretendard',
-          fontWeight: '600',
-          lineHeight: 24,
-        },
-        tabBarIndicatorStyle: {
-          backgroundColor: palette.Text.Normal,
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-      }}>
-      <Tab.Screen
-        name={tabBarLabel.DetailInfoScreen}
-        component={DetailInfoScreen}
-      />
-      <Tab.Screen
-        name={tabBarLabel.DetailReviewScreen}
-        component={DetailReviewScreen}
-      />
-    </Tab.Navigator>
+    <View style={{flex: 1}}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: palette.Text.Normal,
+          tabBarInactiveTintColor: palette.Text.Assistive,
+          tabBarStyle: {backgroundColor: palette.Another.White},
+          tabBarLabelStyle: {
+            fontSize: 16,
+            fontFamily: 'Pretendard',
+            fontWeight: '600',
+            lineHeight: 24,
+          },
+          tabBarIndicatorStyle: {
+            backgroundColor: palette.Text.Normal,
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        }}>
+        <Tab.Screen
+          name={tabBarLabel.DetailInfoScreen}
+          component={DetailInfoScreen}
+        />
+        <Tab.Screen
+          name={tabBarLabel.DetailReviewScreen}
+          component={DetailReviewScreen}
+        />
+      </Tab.Navigator>
+
+      <View>
+        <DetailBottomButton onPress={() => {}} />
+      </View>
+    </View>
   );
 };
 export default DetailScreen;
---
+++ src/screens/screening/detail/tabs/DetailInfoScreen.style.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const detailInfoStyles = StyleSheet.create({
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 4,
+  },
+});
