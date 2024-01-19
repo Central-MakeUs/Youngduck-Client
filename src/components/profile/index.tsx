944fd0326539839493544159270f124d0f007510
suminhan123 <suminh324@gmail.com>
2024-01-20 01:05:23 +0900

    feat: 프로필 size prop 추가

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -3,14 +3,18 @@ import {profileStyles} from './Profile.style';
 import useNavigator from '@/hooks/useNavigator';
 import {defaultImages} from '@/assets';
 
-const Profile = () => {
+interface ProfileProps {
+  size?: 'small' | 'large';
+}
+
+const Profile = ({size = 'small'}: ProfileProps) => {
   const {stackNavigation} = useNavigator();
   const handleGoMyPage = () => {
     stackNavigation.navigate('MyPageScreen');
   };
   return (
     <TouchableOpacity activeOpacity={0.8} onPress={handleGoMyPage}>
-      <Image source={defaultImages.profile1} style={profileStyles.image} />
+      <Image source={defaultImages.profile1} style={profileStyles[size]} />
     </TouchableOpacity>
   );
 };
