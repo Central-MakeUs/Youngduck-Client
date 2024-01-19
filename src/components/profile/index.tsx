a0408144abea22ec07bedfb4a8446f08e059714e
suminhan123 <suminh324@gmail.com>
2024-01-20 00:33:23 +0900

    design: 프로필 이미지 추가

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,6 +1,7 @@
 import {Image, TouchableOpacity} from 'react-native';
 import {profileStyles} from './Profile.style';
 import useNavigator from '@/hooks/useNavigator';
+import {defaultImages} from '@/assets';
 
 const Profile = () => {
   const {stackNavigation} = useNavigator();
@@ -9,13 +10,7 @@ const Profile = () => {
   };
   return (
     <TouchableOpacity activeOpacity={0.8} onPress={handleGoMyPage}>
-      <Image
-        source={{
-          //임의로 uri 넣어줌 => TODO: 유저 uri 값 zustand 꺼내기
-          uri: 'https://cdn.pixabay.com/photo/2023/12/27/14/37/ai-generated-8472201_1280.png',
-        }}
-        style={profileStyles.image}
-      />
+      <Image source={defaultImages.profile1} style={profileStyles.image} />
     </TouchableOpacity>
   );
 };
