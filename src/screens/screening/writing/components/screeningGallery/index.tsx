4925414beb06557ba31a37ddbf3a42697c483291
suminhan123 <98216274+suminhan123@users.noreply.github.com>
2024-01-20 16:15:00 +0900

    [FEAT] 갤러리 권한 및 갤러리 이미지 업로드 (#40)
    * chore: 푸쉬알림, 갤러리 권한 안드로이드 세팅
    
    * feat: 접근 권한 util 생성
    
    * feat: 작성하기 페이지 카메라 권한 로직 추가
    
    * feat: 갤러리 이미지 경로 가져오기
    
    * comment: TODO 주석 추가
    
    * feat: iOS 갤러리 권한 설정
    
    * remove: 기존 권한 함수 삭제
    
    * feat: 권한 로직 수정
    
    ---------
    
    Co-authored-by: gs0428 <lider28@naver.com>

--- src/screens/screening/writing/components/screeningGallery/index.tsx
+++ src/screens/screening/writing/components/screeningGallery/index.tsx
@@ -1,12 +1,45 @@
 import Typography from '@/components/typography';
 import palette from '@/styles/theme/color';
 import Gallery from '@/assets/icons/gallery.svg';
-import {Image, View} from 'react-native';
+import {Image, TouchableOpacity, View} from 'react-native';
 import {galleryStyles} from './ScreeningGallery.style';
 import RoundButton from '@/components/buttons/roundButton';
 import SvgIcons from '@/assets/svgIcons';
+import ImageCropPicker from 'react-native-image-crop-picker';
+import {checkPermission} from '@/utils/checkPermission';
 
 const ScreeningGallery = () => {
+  const handleImageUpload = async () => {
+    try {
+      const image = await ImageCropPicker.openPicker({
+        mediaType: 'photo',
+        includeBase64: true,
+        width: 120,
+        height: 120,
+      });
+      if (image?.path) {
+        console.log('이미지 경로', image.path);
+        // TODO: 백엔드 api 통신
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleClickGallery = async () => {
+    // 갤러리 이미지 접근 권한 허용
+
+    try {
+      const granted = await checkPermission();
+      if (granted) {
+        handleImageUpload();
+      } else {
+        console.log('권한이 거부되었습니다.');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <View>
       <Typography style="Label2" color={palette.Text.Strong} mb={4}>
@@ -16,10 +49,13 @@ const ScreeningGallery = () => {
         <Image
           source={{
             uri: 'https://cdn.pixabay.com/photo/2017/07/13/23/11/cinema-2502213_1280.jpg',
+            // TODO: 백엔드 api 응답 uri 넣기
           }}
           style={galleryStyles.image}
         />
-        <Gallery />
+        <TouchableOpacity onPress={handleClickGallery} activeOpacity={0.8}>
+          <Gallery />
+        </TouchableOpacity>
       </View>
       <View style={galleryStyles.proContainer}>
         <Typography style="Body2" color={palette.Text.Alternative}>
