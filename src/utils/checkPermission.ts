8e5f1e4814995f4aefc32834c3809ba2a39649eb
suminhan123 <98216274+suminhan123@users.noreply.github.com>
2024-01-21 00:35:08 +0900

    [REFACTOR] 갤러리 접근 및 이미지 업로드 로직 리팩토링 (#50)
    * refactor: 권한 로직 리팩토링
    
    * feat: 권한 허용 시 바로 image crop 열기 추가

--- src/utils/checkPermission.ts
+++ src/utils/checkPermission.ts
@@ -1,54 +1,40 @@
 import {Alert, Linking, Platform} from 'react-native';
-import {check, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 
-// 갤러리 접근 권한 허용 체크
-export const checkPermission = async () => {
+// 갤러리 접근 권한 허용 체크 => 갤러리 여는 함수를 인자로 전달
+export const checkPermission = async (ImageCrop: () => void) => {
+  const galleryPermission =
+    Platform.OS === 'android'
+      ? PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
+      : PERMISSIONS.IOS.PHOTO_LIBRARY;
   try {
-    if (Platform.OS === 'android') {
-      const result = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-      if (result === RESULTS.BLOCKED || result === RESULTS.DENIED) {
-        Alert.alert(
-          '이미지를 업로드 하려면 갤러리 권한 허용이 필요합니다.',
-          '앱 설정 화면을 열어서 항상 허용으로 바꿔주세요.',
-          [
-            {
-              text: '네',
-              onPress: () => Linking.openSettings(),
-            },
-            {
-              text: '아니오',
-              onPress: () => console.log('No Pressed'),
-              style: 'cancel',
-            },
-          ],
-        );
-        return false;
-      }
+    const result = await check(galleryPermission);
+    if (result === RESULTS.GRANTED) {
       return true;
-    } else if (Platform.OS === 'ios') {
-      const result = await check(PERMISSIONS.IOS.PHOTO_LIBRARY);
-      if (result === RESULTS.BLOCKED || result === RESULTS.DENIED) {
-        Alert.alert(
-          '이미지를 업로드 하려면 갤러리 권한 허용이 필요합니다.',
-          '앱 설정 화면을 열어서 항상 허용으로 바꿔주세요.',
-          [
-            {
-              text: '네',
-              onPress: () => Linking.openSettings(),
-            },
-            {
-              text: '아니오',
-              onPress: () => console.log('No Pressed'),
-              style: 'cancel',
-            },
-          ],
-        );
-        return false;
-      }
-      return true;
-    } else {
-      console.log('디바이스를 찾을 수 없습니다');
     }
+
+    request(galleryPermission).then(res => {
+      if (res === 'blocked') {
+        Alert.alert(
+          '이미지를 업로드 하려면 갤러리 권한 허용이 필요합니다.',
+          '앱 설정 화면을 열어서 항상 허용으로 바꿔주세요.',
+          [
+            {
+              text: '네',
+              onPress: () => Linking.openSettings(),
+            },
+            {
+              text: '아니오',
+              onPress: () => console.log('No Pressed'),
+              style: 'cancel',
+            },
+          ],
+        );
+        return false;
+      }
+      ImageCrop();
+      return false;
+    });
   } catch (e) {
     console.log('에러 발생', e);
     return false;
