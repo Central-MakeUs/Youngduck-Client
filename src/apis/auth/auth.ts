3b88e724ee1ff57240c133e50158fc5f93aa4489
suminhan123 <suminh324@gmail.com>
2024-01-21 18:23:13 +0900

    feat: accessToken 재발급 api 함수 생성

--- src/apis/auth/auth.ts
+++ src/apis/auth/auth.ts
@@ -3,6 +3,7 @@ import {apiWithoutToken} from '@/apis';
 import {IRegisterRequest} from '@/models/auth/request';
 import {ResponseDTO} from '@/models/common/responseDTO';
 import {ILoginResponse, IRegisterResponse} from '@/models/auth/response';
+import {setTokens} from '@/services/localStorage/localStorage';
 
 // 로그인 api 함수
 export const postLoginUser = async (
@@ -26,4 +27,21 @@ export const postRegisterUser = async (
     body,
   );
   return res.data;
+};
+
+// refresh 토큰으로 access 토큰 재발급 api 함수
+export const postAccessToken = async (
+  refreshToken: string,
+): Promise<boolean> => {
+  try {
+    const res = await apiWithoutToken.post(
+      `/auth/token/refresh?refreshToken=${refreshToken}`,
+    );
+    console.log(res.data);
+    await setTokens(res.data.data.refreshToken, res.data.data.accessToken);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 };
