6916ebe4e8e8d217ad4c737046d4655f82bfac19
suminhan123 <suminh324@gmail.com>
2024-01-21 20:27:40 +0900

    feat: axios interceptor 재발급 및 헤더 토큰  로직 생성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,12 @@
-import axios from 'axios';
+import {ResponseErrorAPI} from '@/models/common/responseDTO';
+import {getAccessToken} from '@react-native-seoul/kakao-login';
+import axios, {AxiosError, AxiosResponse} from 'axios';
 import Config from 'react-native-config';
+import {postAccessToken} from './auth/auth';
 
 const baseURL = Config.BASE_URL;
 
+// 토큰 없이 호출하는 api instance
 export const apiWithoutToken = axios.create({
   baseURL,
   timeout: 10 * 1000,
@@ -11,3 +15,64 @@ export const apiWithoutToken = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// 헤더에 토큰 넣어서 호출하는 api instance
+export const api = axios.create({
+  baseURL,
+  headers: {
+    Accept: 'application/json;charset=UTF-8',
+    'Content-Type': 'application/json',
+  },
+});
+
+// api 함수 호출 전에 실행
+api.interceptors.request.use(
+  async config => {
+    // 로컬 스토리지에서 access token을 꺼냄
+    const accessToken = await getAccessToken();
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+// api 함수 호출 후 성공했을 시에 실행
+const onFulfilled = (res: AxiosResponse) => res;
+
+// api 함수 호출 후 실패했을 시에 실행
+let lock = false;
+
+const onRejected = async (error: AxiosError) => {
+  const originalConfig = error.config;
+  const data = error.response?.data as ResponseErrorAPI;
+
+  if (
+    originalConfig &&
+    error.response?.status === 401 &&
+    data?.code == 'AUTH_403_1' &&
+    !lock
+  ) {
+    lock = true;
+    try {
+      await postAccessToken();
+      return apiWithoutToken
+        .request({
+          ...originalConfig,
+          headers: {
+            Authorization: `Bearer ${await getAccessToken()}`,
+          },
+        })
+        .finally(() => (lock = false));
+    } catch (error) {
+      console.log('axios interceptor error: ', error);
+    }
+  }
+  return Promise.reject(error);
+};
+api.interceptors.response.use(onFulfilled, onRejected);
