ba1655d8a1638327cefe74a71ebe20bb3501a160
gs0428 <lider28@naver.com>
2024-01-22 16:34:30 +0900

    feat: 뒤로가기, title, 닫기 있는 top bar 구현

---
+++ src/components/topBar/backCancelTopBar/index.tsx
@@ -0,0 +1,35 @@
+import SvgIcons from '@/assets/svgIcons';
+
+import {CommonMarginVerticalProps} from '@/types/ui';
+import {View} from 'react-native';
+import Typography from '@/components/typography';
+import palette from '@/styles/theme/color';
+import useNavigator from '@/hooks/useNavigator';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import backCancelTopBarStyles from './BackCancelTopBar.style';
+
+interface BackTitleTopBarProps extends CommonMarginVerticalProps {
+  onPress: () => void;
+  text: string;
+}
+
+const BackCancelTopBar = ({onPress, text, mb, mt}: BackTitleTopBarProps) => {
+  const {stackNavigation} = useNavigator();
+  const {top} = useSafeAreaInsets();
+  const style = backCancelTopBarStyles({top});
+  return (
+    <View
+      style={{
+        ...style.container,
+        marginTop: mt ? mt : undefined,
+        marginBottom: mb ? mb : undefined,
+      }}>
+      <SvgIcons.BackArrowIcon onPress={onPress} />
+      <Typography style="Label1" color={palette.Another.Black}>
+        {text}
+      </Typography>
+      <SvgIcons.CancelIcon onPress={stackNavigation.goBack} />
+    </View>
+  );
+};
+export default BackCancelTopBar;
