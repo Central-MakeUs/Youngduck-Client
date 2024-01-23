576acc9087724d7fcd3cb03131eb82eb8f9ccecd
suminhan123 <suminh324@gmail.com>
2024-01-23 17:34:01 +0900

    design: 디테일 타이틀 디자인 변경 반영

--- src/screens/screening/detail/components/detailTitle/DetailTitle.style.ts
+++ src/screens/screening/detail/components/detailTitle/DetailTitle.style.ts
@@ -3,9 +3,6 @@ import {StyleSheet} from 'react-native';
 export const detailTitleStyles = StyleSheet.create({
   container: {
     paddingHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     paddingTop: 16,
     paddingBottom: 7,
   },
--- src/screens/screening/detail/components/detailTitle/index.tsx
+++ src/screens/screening/detail/components/detailTitle/index.tsx
@@ -1,4 +1,3 @@
-import OptionButton from '@/components/buttons/optionButton';
 import Typography from '@/components/typography';
 import palette from '@/styles/theme/color';
 import {View} from 'react-native';
@@ -7,15 +6,12 @@ import {detailTitleStyles} from './DetailTitle.style';
 const DetailTitle = () => {
   return (
     <View style={detailTitleStyles.container}>
-      <View>
-        <Typography style="Label2" color={palette.Text.Normal}>
-          상영전
-        </Typography>
-        <Typography style="Title2" color={palette.Text.Normal}>
-          Dromapic 상영회
-        </Typography>
-      </View>
-      <OptionButton type="alram" />
+      <Typography style="Label2" color={palette.Text.Normal}>
+        상영전
+      </Typography>
+      <Typography style="Title2" color={palette.Text.Normal}>
+        Dromapic 상영회
+      </Typography>
     </View>
   );
 };
