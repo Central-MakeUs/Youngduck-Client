0fbf152e494fa0c714351cc22a7d7d1b2a0f3120
gs0428 <lider28@naver.com>
2024-01-23 20:43:19 +0900

    feat: 닉네임 변경 1차 화면 구현

---
+++ src/screens/myPage/changeNickname/ChangeNicknameScreen.tsx
@@ -0,0 +1,53 @@
+import SvgIcons from '@/assets/svgIcons';
+import DefaultContainer from '@/components/container/defaultContainer';
+import Typography from '@/components/typography';
+import useNavigator from '@/hooks/useNavigator';
+import InputNickname from '@/screens/signup/components/inputNickname';
+import palette from '@/styles/theme/color';
+import {useState} from 'react';
+import {View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+interface IChangeNicknameScreenProp {
+  route: {
+    params: {
+      nickname: string;
+    };
+  };
+}
+
+const ChangeNicknameScreen = ({route}: IChangeNicknameScreenProp) => {
+  const [nickname, setNickname] = useState<string>(route.params.nickname);
+  const {stackNavigation} = useNavigator();
+  const {top, bottom} = useSafeAreaInsets();
+  return (
+    <>
+      <View
+        style={{
+          flexDirection: 'row',
+          padding: 16,
+          paddingTop: 16 + top,
+          alignItems: 'center',
+        }}>
+        <SvgIcons.BackArrowIcon onPress={stackNavigation.goBack} />
+        <View style={{flex: 1, alignItems: 'center', paddingRight: 24}}>
+          <Typography style="Label1" color={palette.Another.Black}>
+            닉네임 변경하기
+          </Typography>
+        </View>
+      </View>
+      <DefaultContainer>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'space-between',
+            paddingBottom: bottom + 16,
+          }}>
+          <Typography style="Subtitle2">수정할 닉네임</Typography>
+        </View>
+      </DefaultContainer>
+    </>
+  );
+};
+
+export default ChangeNicknameScreen;
