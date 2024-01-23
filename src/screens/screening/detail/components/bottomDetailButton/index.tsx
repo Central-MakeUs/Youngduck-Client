7f58fb9df3d0264c5f6960810ec66fc6dcb69287
suminhan123 <suminh324@gmail.com>
2024-01-24 00:02:32 +0900

    refactor: 스크리닝 디테일 bottom button 컴포넌트 리팩토링

--- src/screens/screening/detail/components/bottomDetailButton/index.tsx
+++ src/screens/screening/detail/components/bottomDetailButton/index.tsx
@@ -13,90 +13,103 @@ interface IBottomButtonProps {
   onPress: () => void;
   type?: 'default' | 'complete' | 'finish' | 'reviewStart' | 'reviewComplete';
   onOptionPress?: () => void;
+  heartState?: boolean;
 }
 
+interface IBottomDetail {
+  text: string;
+  color: string;
+  disabled: boolean;
+  icon: React.ReactNode;
+  heartDisabled: boolean;
+}
+
+const bottomDetailTypesStyles: Record<
+  'complete' | 'reviewStart' | 'reviewComplete',
+  IBottomDetail
+> = {
+  complete: {
+    text: '신청완료',
+    color: palette.Another.White,
+    disabled: true,
+    icon: <WhiteTicket />,
+    heartDisabled: false,
+  },
+  reviewStart: {
+    text: '리뷰하기',
+    color: palette.Text.Normal,
+    disabled: false,
+    icon: <SvgIcons.Pencil fill={palette.Text.Normal} />,
+    heartDisabled: true,
+  },
+  reviewComplete: {
+    text: '리뷰완료',
+    color: palette.Another.White,
+    disabled: true,
+    icon: <SvgIcons.Pencil fill={palette.Another.White} />,
+    heartDisabled: true,
+  },
+};
 const BottomDetailButton = ({
   onPress,
   onOptionPress,
   type = 'default',
+  heartState,
 }: IBottomButtonProps) => {
   return (
     <View style={bottomButtonStyles.container}>
-      {type === 'complete' && (
-        <>
-          <View style={bottomButtonStyles.left}>
-            <BoxButton onPress={onPress} disabled={true}>
+      {(() => {
+        if (type === 'default' || type === 'finish') {
+          return (
+            <BoxButton onPress={onPress} disabled={type === 'finish'}>
               <View style={bottomButtonStyles.content}>
-                <Typography style="Label1" color={palette.Another.White} mr={8}>
-                  신청완료
+                <Typography
+                  style="Label1"
+                  color={
+                    type === 'default'
+                      ? palette.Text.Normal
+                      : palette.Another.White
+                  }
+                  mr={8}>
+                  {type === 'default' ? '관람신청' : '상영종료'}
                 </Typography>
-                <WhiteTicket />
+                {type === 'default' ? <BlackTicket /> : <WhiteTicket />}
               </View>
             </BoxButton>
-          </View>
-          <View style={bottomButtonStyles.right}>
-            <OptionButton
-              type="heart"
-              isSelected={true}
-              onPress={onOptionPress}
-            />
-          </View>
-        </>
-      )}
-      {type === 'reviewStart' && (
-        <>
-          <View style={bottomButtonStyles.left}>
-            <BoxButton onPress={onPress}>
-              <View style={bottomButtonStyles.content}>
-                <Typography style="Label1" color={palette.Text.Normal} mr={8}>
-                  리뷰수정
-                </Typography>
-                <SvgIcons.Pencil fill={palette.Text.Normal} />
-              </View>
-            </BoxButton>
-          </View>
-          <View style={bottomButtonStyles.right}>
-            <OptionButton type="heart" isSelected={true} disabled />
-          </View>
-        </>
-      )}
-      {type === 'reviewComplete' && (
-        <>
-          <View style={bottomButtonStyles.left}>
-            <BoxButton onPress={onPress} disabled>
-              <View style={bottomButtonStyles.content}>
-                <Typography style="Label1" color={palette.Another.White} mr={8}>
-                  리뷰완료
-                </Typography>
-                <SvgIcons.Pencil fill={palette.Another.White} />
-              </View>
-            </BoxButton>
-          </View>
-          <View style={bottomButtonStyles.right}>
-            <OptionButton type="heart" isSelected={true} disabled />
-          </View>
-        </>
-      )}
-      {type === 'default' && (
-        <BoxButton onPress={onPress}>
-          <View style={bottomButtonStyles.content}>
-            <Typography style="Label1" color={palette.Text.Normal} mr={8}>
-              관람신청
-            </Typography>
-            <BlackTicket />
-          </View>
-        </BoxButton>
-      )}
-      {type === 'finish' && (
-        <BoxButton onPress={onPress} disabled>
-          <View style={bottomButtonStyles.content}>
-            <Typography style="Label1" color={palette.Another.White} mr={8}>
-              상영종료
-            </Typography>
-            <WhiteTicket />
-          </View>
-        </BoxButton>
-      )}
+          );
+        }
+        return (
+          <>
+            <View style={bottomButtonStyles.left}>
+              <BoxButton
+                onPress={onPress}
+                disabled={bottomDetailTypesStyles[type].disabled}>
+                <View style={bottomButtonStyles.content}>
+                  <Typography
+                    style="Label1"
+                    color={bottomDetailTypesStyles[type].color}
+                    mr={8}>
+                    {bottomDetailTypesStyles[type].text}
+                  </Typography>
+                  {bottomDetailTypesStyles[type].icon}
+                </View>
+              </BoxButton>
+            </View>
+            <View style={bottomButtonStyles.right}>
+              <OptionButton
+                type="heart"
+                isSelected={
+                  bottomDetailTypesStyles[type].heartDisabled
+                    ? true
+                    : heartState
+                }
+                onPress={onOptionPress!}
+                disabled={bottomDetailTypesStyles[type].heartDisabled}
+              />
+            </View>
+          </>
+        );
+      })()}
     </View>
   );
 };
