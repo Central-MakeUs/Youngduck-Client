9832889bf7a3552336813b387b0531663279e54f
suminhan123 <suminh324@gmail.com>
2024-01-25 19:16:02 +0900

    refactor: 디테일 리뷰 반영

--- src/screens/screening/detail/components/detailInfo/index.tsx
+++ src/screens/screening/detail/components/detailInfo/index.tsx
@@ -1,6 +1,5 @@
 import Typography from '@/components/typography';
 import palette from '@/styles/theme/color';
-import {View} from 'react-native';
 
 interface IDetailInfo {
   title: string;
@@ -8,14 +7,14 @@ interface IDetailInfo {
 }
 const DetailInfo = ({title, content}: IDetailInfo) => {
   return (
-    <View>
+    <>
       <Typography style="Label3" color={palette.Text.Normal} mt={8}>
         {title}
       </Typography>
       <Typography style="Body2" color={palette.Text.Normal} mt={4}>
         {content}
       </Typography>
-    </View>
+    </>
   );
 };
 export default DetailInfo;
