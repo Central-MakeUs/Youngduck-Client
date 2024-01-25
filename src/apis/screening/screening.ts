4fae127424e24431c0db1492fc4c08e691fb11af
suminhan123 <suminh324@gmail.com>
2024-01-25 20:33:57 +0900

    feat: 이번주 상영작 api 함수 구현

--- src/apis/screening/screening.ts
+++ src/apis/screening/screening.ts
@@ -1,12 +1,23 @@
 import {api} from '@/apis';
 import {ResponseDTO} from '@/models/common/responseDTO';
 import {IScreeningBodyRequest} from '@/models/screening/request';
-import {IScreeningDetailResponse} from '@/models/screening/response';
+import {
+  IScreeningDetailResponse,
+  TWeekScreeningResponse,
+} from '@/models/screening/response';
 
 // 스크리닝 업로드 함수
 export const postScreening = async (
   body: IScreeningBodyRequest,
 ): Promise<ResponseDTO<IScreeningDetailResponse>> => {
   const res = await api.post('/screening/upload-screening', body);
+  return res.data;
+};
+
+// 스크리닝 이번주 스크리닝 함수
+export const getWeekScreening = async (): Promise<
+  ResponseDTO<TWeekScreeningResponse>
+> => {
+  const res = await api.get('/screening/upcoming-Screening');
   return res.data;
 };
