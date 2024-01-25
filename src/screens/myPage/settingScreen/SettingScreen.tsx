dd63276645323e2185d670a73b82a226f823bd92
gs0428 <lider28@naver.com>
2024-01-25 15:54:50 +0900

    refactor: swtich component prop 추가

--- src/screens/myPage/settingScreen/SettingScreen.tsx
+++ src/screens/myPage/settingScreen/SettingScreen.tsx
@@ -1,8 +1,15 @@
+import Divider from '@/components/divider';
+import Switch from '@/components/switch';
+import SubTitle from '@/components/title/subTitle';
+import SubTitleDescription from '@/components/title/subTitleDescription';
 import BackTitleTopBar from '@/components/topBar/backTitleTopBar';
 import useNavigator from '@/hooks/useNavigator';
+import {useState} from 'react';
+import {View} from 'react-native';
 
 const SettingScreen = () => {
   const {stackNavigation} = useNavigator();
+  const [isAlarmOn, setIsAlarmOn] = useState<boolean>(false);
   return (
     <>
       <BackTitleTopBar
@@ -10,6 +17,26 @@ const SettingScreen = () => {
         goBack={stackNavigation.goBack}
         opacity={0}
       />
+      <SubTitle text="알림 설정" />
+      <View
+        style={{
+          paddingHorizontal: 16,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <View>
+          <SubTitleDescription
+            text="푸시 알림"
+            subTitle={`관심 스크리닝 상영일 하루 전\n스케줄 알림을 드려요.`}
+          />
+        </View>
+        <Switch
+          onPress={() => setIsAlarmOn(!isAlarmOn)}
+          isOn={isAlarmOn}
+          mt={16}
+        />
+      </View>
+      <Divider height={8} mt={16} mb={16} />
     </>
   );
 };
