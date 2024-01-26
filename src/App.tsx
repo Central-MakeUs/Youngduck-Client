59e075c6d22a261c0aa4398dbdb3aa0350466390
suminhan123 <suminh324@gmail.com>
2024-01-27 02:27:55 +0900

    feat: device 토큰 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import {useEffect} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import 'react-native-svg';
 
 import Navigator from './navigators/Navigator';
-import {setupAlarm} from './services/alarmService';
+import {getDeviceToken, setupAlarm} from './services/alarmService';
 
 setupAlarm();
 
 function App() {
+  useEffect(() => {
+    getDeviceToken();
+  }, []);
   const queryClient = new QueryClient();
 
   return (
--- src/services/alarmService.ts
+++ src/services/alarmService.ts
@@ -2,7 +2,7 @@ import messaging from '@react-native-firebase/messaging';
 import PushNotification from 'react-native-push-notification';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
-export const setupAlarm = () => {
+const setupAlarm = () => {
   messaging().setBackgroundMessageHandler(async remoteMessage => {
     console.log('Message handled in the background!', remoteMessage);
   });
@@ -18,6 +18,7 @@ export const setupAlarm = () => {
       // ios 설정
       notification.finish(PushNotificationIOS.FetchResult.NoData);
     },
+
     // notification에 action 등록이 가능
     // 등록한 액션을 누렀고 invokeApp이 false 상태일 때 실행
     onAction: function (notification: any) {
@@ -37,4 +38,32 @@ export const setupAlarm = () => {
     // 권한 요청
     requestPermissions: true,
   });
+
+  // 알람 기본 설정 및 채널 구성
+  PushNotification.createChannel(
+    {
+      channelId: 'screening',
+      channelName: '앱 전반',
+      channelDescription: '앱 실행하는 알림',
+      soundName: 'default',
+      importance: 4,
+      vibrate: true,
+    },
+    (created: boolean) => console.log(`채널 생성 성공 '${created}'`),
+  );
 };
+
+const getDeviceToken = async () => {
+  try {
+    if (!messaging().isDeviceRegisteredForRemoteMessages) {
+      // 기기 등록
+      await messaging().registerDeviceForRemoteMessages();
+    }
+    const token = await messaging().getToken();
+    console.log('phone token', token);
+    // 서버로 부터 토큰 api로 보내줌
+  } catch (error) {
+    console.error(error);
+  }
+};
+export {setupAlarm, getDeviceToken};
