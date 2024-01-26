480a3e4958f35ede8290191624b6c3bf7cb4d777
suminhan123 <suminh324@gmail.com>
2024-01-26 16:02:53 +0900

    design: carousel 디자인 수정

--- src/screens/screening/home/components/reviewItem/index.tsx
+++ src/screens/screening/home/components/reviewItem/index.tsx
@@ -1,11 +1,12 @@
 import {Image, TouchableOpacity, View} from 'react-native';
-import {reviewItemStyles} from './ReviewItem.style';
+
 import Typography from '@/components/typography';
 import ChatIcon from '@/assets/icons/chat.svg';
-import DefaultContainer from '@/components/container/defaultContainer';
 import useNavigator from '@/hooks/useNavigator';
 import palette from '@/styles/theme/color';
 import stackScreens from '@/constants/stackScreens';
+
+import {reviewItemStyles} from './ReviewItem.style';
 
 const ReviewItem = () => {
   const {stackNavigation} = useNavigator();
@@ -14,36 +15,34 @@ const ReviewItem = () => {
     stackNavigation.navigate(stackScreens.DetailScreen, {id: 1});
   };
   return (
-    <DefaultContainer>
-      <TouchableOpacity
-        style={reviewItemStyles.container}
-        activeOpacity={0.8}
-        onPress={handleGoDetail}>
-        <Image
-          source={{
-            uri: 'https://cdn.pixabay.com/photo/2017/07/13/23/11/cinema-2502213_1280.jpg',
-          }}
-          style={reviewItemStyles.image}
-        />
-        <View style={reviewItemStyles.content}>
-          <Typography style="Label2">영화제</Typography>
-          <Typography style="Body1" mb={8}>
-            부산영화제
+    <TouchableOpacity
+      style={reviewItemStyles.container}
+      activeOpacity={0.8}
+      onPress={handleGoDetail}>
+      <Image
+        source={{
+          uri: 'https://cdn.pixabay.com/photo/2017/07/13/23/11/cinema-2502213_1280.jpg',
+        }}
+        style={reviewItemStyles.image}
+      />
+      <View style={reviewItemStyles.content}>
+        <Typography style="Label2">영화제</Typography>
+        <Typography style="Body1" mb={8}>
+          부산영화제
+        </Typography>
+        <View style={reviewItemStyles.option}>
+          <Typography style="Body2" color={palette.Text.Alternative}>
+            2024.01.05 - 2024.01.06
           </Typography>
-          <View style={reviewItemStyles.option}>
-            <Typography style="Body2" color={palette.Text.Alternative}>
-              2024.01.05 - 2024.01.06
+          <View style={reviewItemStyles.flex}>
+            <ChatIcon />
+            <Typography style="Body2" color={palette.Text.Alternative} ml={7}>
+              2
             </Typography>
-            <View style={reviewItemStyles.flex}>
-              <ChatIcon />
-              <Typography style="Body2" color={palette.Text.Alternative} ml={7}>
-                2
-              </Typography>
-            </View>
           </View>
         </View>
-      </TouchableOpacity>
-    </DefaultContainer>
+      </View>
+    </TouchableOpacity>
   );
 };
 export default ReviewItem;
