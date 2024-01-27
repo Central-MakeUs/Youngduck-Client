2df336a3f195bc9a46dcdd0b9ba1fd98d95d4b2f
gs0428 <lider28@naver.com>
2024-01-27 16:39:52 +0900

    feat: iOS 알람 설정

--- ios/Youngduck/AppDelegate.mm
+++ ios/Youngduck/AppDelegate.mm
@@ -1,8 +1,8 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 #import "RNSplashScreen.h"
 
 #import <React/RCTBundleURLProvider.h>
-
 #import <RNKakaoLogins.h>
 
 
@@ -19,6 +19,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) { // 추가 (line:35)
+    [FIRApp configure];
+  }
   self.moduleName = @"Youngduck";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
