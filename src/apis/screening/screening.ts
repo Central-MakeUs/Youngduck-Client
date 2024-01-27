920ca4c97bb19791fc79b33f95d41813a9713c1a
suminhan123 <suminh324@gmail.com>
2024-01-27 21:38:34 +0900

    feat: screening search api 함수 작성

--- src/apis/screening/screening.ts
+++ src/apis/screening/screening.ts
@@ -3,6 +3,7 @@ import {ResponseDTO} from '@/models/common/responseDTO';
 import {
   IScreeningBodyRequest,
   IScreeningListOptionBodyRequest,
+  IScreeningListSearchBodyRequest,
 } from '@/models/screening/request';
 import {
   IScreeningDetailResponse,
@@ -47,6 +48,16 @@ export const getTimeOptionScreeningList = async (
 ) => {
   const res = await api.get(
     `/screening/screenings/search-by-date?category=${body.category}&page=${body.page}&size=${body.size}&sortBy=${body.sortBy}`,
+  );
+  return res.data;
+};
+
+// 스크리닝 목록 검색 함수
+export const getSearchScreeningList = async (
+  body: IScreeningListSearchBodyRequest,
+) => {
+  const res = await api.get(
+    `/screening/screenings/search?title=${body.title}&category=${body.category}&page=${body.page}&size=${body.size}`,
   );
   return res.data;
 };
