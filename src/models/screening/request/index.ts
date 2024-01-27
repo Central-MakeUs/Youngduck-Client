b004807f611b5ff17b3b2fab771b39bf9f5210aa
suminhan123 <suminh324@gmail.com>
2024-01-28 01:28:01 +0900

    refactor: 공통 시간 타입 파일 생성

--- src/models/screening/request/index.ts
+++ src/models/screening/request/index.ts
@@ -1,6 +1,7 @@
 import {DateParsable} from 'react-native-calendar-picker';
 
 import {TEngCategory, TKorCategory} from '@/models/enums/category';
+import {TScreeningTimeOption} from '@/models/enums/screeningOption';
 
 // 스크리닝 등록하기 요청 body 타입
 export interface IScreeningBodyRequest {
@@ -27,7 +28,7 @@ export interface IScreeningListCommon {
 }
 // 스크리닝 목록 옵션 요청 body 타입
 export interface IScreeningListOptionBodyRequest extends IScreeningListCommon {
-  sortBy: 'createdAt' | 'startDate';
+  sortBy: TScreeningTimeOption;
 }
 
 // 스크리닝 목록 검색 요청 body 타입
