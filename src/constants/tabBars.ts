59c412a7eaa149084a2c9cc62a313813f73092e9
suminhan123 <suminh324@gmail.com>
2024-01-28 20:49:58 +0900

    refactor: 탭바 상수 생성

---
+++ src/constants/tabBars.ts
@@ -0,0 +1,12 @@
+// 스크리닝 디테일 탭바 상수
+export const screeningTabBars = [
+  {title: '스크리닝 정보', tabNumber: 0},
+  {title: '리뷰', tabNumber: 1},
+];
+
+// 스크리닝 본인이 작성한 디테일 탭바 상수
+export const myScreeningTabBars = [
+  {title: '스크리닝 정보', tabNumber: 0},
+  {title: '리뷰', tabNumber: 1},
+  {title: '통계', tabNumber: 2},
+];
