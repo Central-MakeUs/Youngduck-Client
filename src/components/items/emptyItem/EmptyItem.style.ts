97be2f95a0c54d334d1b21401a4156216218c443
suminhan123 <suminh324@gmail.com>
2024-01-28 21:53:11 +0900

    design: empty 아이템 다자인 수정

--- src/components/items/emptyItem/EmptyItem.style.ts
+++ src/components/items/emptyItem/EmptyItem.style.ts
@@ -3,8 +3,6 @@ import {StyleSheet} from 'react-native';
 export const emptyItemStyles = StyleSheet.create({
   container: {
     width: '100%',
-    paddingTop: 16,
-    paddingBottom: 24,
     alignItems: 'center',
     justifyContent: 'center',
   },
