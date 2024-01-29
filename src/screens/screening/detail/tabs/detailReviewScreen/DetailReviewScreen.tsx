8843753d6836a5755e9aedf84c02e1cf875e97ca
suminhan123 <suminh324@gmail.com>
2024-01-30 02:32:28 +0900

    feat: 리뷰 리스트 5개 더보기 로직 추가

--- src/screens/screening/detail/tabs/detailReviewScreen/DetailReviewScreen.tsx
+++ src/screens/screening/detail/tabs/detailReviewScreen/DetailReviewScreen.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {View} from 'react-native';
 import {useQuery} from '@tanstack/react-query';
 
@@ -5,9 +6,10 @@ import DefaultContainer from '@/components/container/defaultContainer';
 import CommentItem from '@/components/items/commentItem';
 import BoxButton from '@/components/buttons/boxButton';
 import Typography from '@/components/typography';
+import LoadingPage from '@/components/pages/loadingPage';
+import EmptyItem from '@/components/items/emptyItem';
 import palette from '@/styles/theme/color';
 import {getScreeningDetailReview} from '@/apis/screening/review';
-import EmptyItem from '@/components/items/emptyItem';
 import {getSimpleDate} from '@/utils/getDate';
 
 import {reviewScreenStyles} from './DetailReviewScreen.style';
@@ -16,33 +18,47 @@ interface IDetailReviewProps {
   id: number;
 }
 const DetailReviewScreen = ({id}: IDetailReviewProps) => {
-  const {data, status} = useQuery({
+  const [moreComment, setMoreComment] = useState<boolean>(false);
+  const {data, status, isLoading} = useQuery({
     queryKey: ['screeningReview'],
     queryFn: () => getScreeningDetailReview(id),
   });
-  //console.log('응답', data);
 
   const reviewList = status === 'success' ? data.data : [];
 
+  if (isLoading) {
+    return (
+      <View style={{height: 350}}>
+        <LoadingPage />
+      </View>
+    );
+  }
+
+  if (reviewList.length === 0) {
+    return (
+      <View
+        style={{
+          height: 350,
+        }}>
+        <EmptyItem size="large" text="댓글이 아직 없습니다" />
+      </View>
+    );
+  }
   return (
-    <View>
-      <DefaultContainer>
-        {reviewList.length === 0 && (
-          <View style={{height: 300}}>
-            <EmptyItem size="large" text="댓글이 아직 없습니다" />
+    <DefaultContainer>
+      {reviewList && (
+        <>
+          <View style={reviewScreenStyles.title}>
+            <Typography style="Subtitle2" color={palette.Another.Black}>
+              관객 리뷰
+            </Typography>
+            <Typography style="Label1" color={palette.Primary.Deep} ml={8}>
+              {reviewList.length.toString()}
+            </Typography>
           </View>
-        )}
-        {reviewList && reviewList.length > 0 && (
-          <>
-            <View style={reviewScreenStyles.title}>
-              <Typography style="Subtitle2" color={palette.Another.Black}>
-                관객 리뷰
-              </Typography>
-              <Typography style="Label1" color={palette.Primary.Deep} ml={8}>
-                {reviewList.length.toString()}
-              </Typography>
-            </View>
-            {reviewList.map((comment, idx) => (
+          {reviewList
+            .slice(0, reviewList.length > 4 ? 5 : reviewList.length)
+            .map((comment, idx) => (
               <CommentItem
                 totalComments={reviewList.length}
                 nickname={comment.nickname}
@@ -50,19 +66,37 @@ const DetailReviewScreen = ({id}: IDetailReviewProps) => {
                 review={comment.review}
                 date={getSimpleDate(comment.createdAt)}
                 idx={idx}
-                key={comment.reviewId}
+                key={idx}
               />
             ))}
-
+          {reviewList.length > 5 && !moreComment ? (
             <View style={reviewScreenStyles.bottom}>
-              <BoxButton onPress={() => {}} variant="default">
+              <BoxButton
+                onPress={() => {
+                  setMoreComment(true);
+                }}
+                variant="default">
                 더보기
               </BoxButton>
             </View>
-          </>
-        )}
-      </DefaultContainer>
-    </View>
+          ) : (
+            <>
+              {reviewList.slice(5).map((comment, idx) => (
+                <CommentItem
+                  totalComments={reviewList.length}
+                  nickname={comment.nickname}
+                  isSatisfied={comment.afterScreening}
+                  review={comment.review}
+                  date={getSimpleDate(comment.createdAt)}
+                  idx={idx}
+                  key={idx}
+                />
+              ))}
+            </>
+          )}
+        </>
+      )}
+    </DefaultContainer>
   );
 };
 export default DetailReviewScreen;
