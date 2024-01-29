f9ba0f765c1f9e46bfc5e790e8ce24d5ae442487
suminhan123 <suminh324@gmail.com>
2024-01-29 20:15:43 +0900

    feat: 스크리닝 웹뷰 페이지 벗어날 경우 이벤트 감지

--- src/screens/screening/detailWebview/DetailWebviewScreen.tsx
+++ src/screens/screening/detailWebview/DetailWebviewScreen.tsx
@@ -1,14 +1,32 @@
-import {View} from 'react-native';
-
-import {getScreenSize} from '@/utils/getScreenSize';
+import stackScreens from '@/constants/stackScreens';
+import useNavigator from '@/hooks/useNavigator';
+import {useWebviewStore} from '@/stores/webview';
+import {ScreenRouteProp} from '@/types/navigator';
+import {useEffect} from 'react';
+import {BackHandler} from 'react-native';
 import WebView from 'react-native-webview';
 
-const DetailWebviewScreen = () => {
-  const {screenHeight, screenWidth} = getScreenSize();
-  return (
-    <View style={{height: screenHeight, width: screenWidth}}>
-      <WebView source={{uri: 'https://www.naver.com'}} style={{flex: 1}} />
-    </View>
-  );
+interface IDetailWebviewScreen {
+  route: ScreenRouteProp<stackScreens.DetailWebviewScreen>;
+}
+const DetailWebviewScreen = ({route: {params}}: IDetailWebviewScreen) => {
+  const {stackNavigation} = useNavigator();
+  const {setWebviewIsVisited} = useWebviewStore();
+
+  useEffect(() => {
+    const handleBackPress = () => {
+      setWebviewIsVisited(true);
+      stackNavigation.navigate(stackScreens.DetailScreen, {id: params.id});
+      return true;
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    };
+  }, [params.id, setWebviewIsVisited, stackNavigation]);
+  // params.uri
+  return <WebView source={{uri: 'https://www.naver.com'}} style={{flex: 1}} />;
 };
 export default DetailWebviewScreen;
