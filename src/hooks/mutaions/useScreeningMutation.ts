79eafaf99d3553c1bfe5182a575e71a1f995164d
suminhan123 <suminh324@gmail.com>
2024-01-30 02:38:41 +0900

    feat: 스크리닝 리뷰 react query 문 작성

--- src/hooks/mutaions/useScreeningMutation.ts
+++ src/hooks/mutaions/useScreeningMutation.ts
@@ -5,7 +5,10 @@ import {postImageUpload} from '@/apis/image/image';
 import {postScreening} from '@/apis/screening/screening';
 import useNavigator from '../useNavigator';
 import stackScreens from '@/constants/stackScreens';
-import {postScreeningDetailReview} from '@/apis/screening/review';
+import {
+  postScreeningComplainReview,
+  postScreeningDetailReview,
+} from '@/apis/screening/review';
 import {postScreeningBookmark} from '@/apis/screening/detail';
 import {ResponseErrorAPI} from '@/models/common/responseDTO';
 import {showSnackBar} from '@/utils/showSnackBar';
@@ -36,7 +39,6 @@ const useScreeningMutation = () => {
   const uploadScreeningReview = useMutation({
     mutationFn: postScreeningDetailReview,
     onSuccess: () => {
-      console.log('성공');
       queryClient.invalidateQueries({queryKey: ['screeningReview']});
       queryClient.invalidateQueries({queryKey: ['screeningDetail']});
     },
@@ -59,11 +61,20 @@ const useScreeningMutation = () => {
     },
   });
 
+  // 스크리닝 신고 post
+  const complainScreeningReview = useMutation({
+    mutationFn: postScreeningComplainReview,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['screeningReview']});
+    },
+  });
+
   return {
     uploadImage,
     uploadScreening,
     uploadScreeningReview,
     uploadScreeningBookmark,
+    complainScreeningReview,
   };
 };
 export default useScreeningMutation;
