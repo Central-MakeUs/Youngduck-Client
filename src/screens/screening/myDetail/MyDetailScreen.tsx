3fe4a490f48acaf8b1d81728b5501f94d87e6ee3
suminhan123 <suminh324@gmail.com>
2024-01-30 16:31:51 +0900

    feat: 옵션 클릭 시 작성 수정으로 navigate

--- src/screens/screening/myDetail/MyDetailScreen.tsx
+++ src/screens/screening/myDetail/MyDetailScreen.tsx
@@ -2,11 +2,29 @@ import {View} from 'react-native';
 
 import {ScreenRouteProp} from '@/types/navigator';
 import stackScreens from '@/constants/stackScreens';
+import MyDetailBottomButton from './components/myDetailBottomButton';
+import useNavigator from '@/hooks/useNavigator';
 
 interface IMyDetailScreenProps {
   route: ScreenRouteProp<stackScreens.MyDetailScreen>;
 }
 const MyDetailScreen = ({route: {params}}: IMyDetailScreenProps) => {
-  return <View></View>;
+  const {stackNavigation} = useNavigator();
+  const handleGoToWrite = () => {
+    stackNavigation.navigate(stackScreens.WritingScreen, {
+      type: 'modified',
+      id: 2,
+      search: 'test',
+    });
+  };
+  return (
+    <View>
+      <MyDetailBottomButton
+        type="myOpen"
+        onPress={() => {}}
+        optionPress={handleGoToWrite}
+      />
+    </View>
+  );
 };
 export default MyDetailScreen;
