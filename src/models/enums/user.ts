ffca3f95f4294fc03fb03281d1c08113b5585fb6
suminhan123 <suminh324@gmail.com>
2024-02-04 16:45:13 +0900

    feat: 탈퇴사유 타입 및 배열 생성

--- src/models/enums/user.ts
+++ src/models/enums/user.ts
@@ -3,3 +3,16 @@ export type TQuitReason =
   | 'UNCONFORTABLE'
   | 'USER_OTHER_SERVICE'
   | 'ETC';
+
+export type TKorQuitReason =
+  | '잘 사용하지 않아요'
+  | '사용성이 불편해요'
+  | '더 나은 서비스를 찾았어요'
+  | '기타';
+
+export const korQuitReason: Array<TKorQuitReason> = [
+  '잘 사용하지 않아요',
+  '사용성이 불편해요',
+  '더 나은 서비스를 찾았어요',
+  '기타',
+];
