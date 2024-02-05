1aca4af4760032ac65956879768f130bd4e76af0
suminhan123 <suminh324@gmail.com>
2024-02-05 19:09:26 +0900

    feat: 쿼리 무효화 후 쿼리 refresh 함수 hook 생성

---
+++ src/hooks/useRefresh.ts
@@ -0,0 +1,35 @@
+import {useCallback, useMemo, useState} from 'react';
+import {QueryKey} from '@tanstack/react-query';
+
+import useInvalidateQueries from './useInvalidateQueries';
+import {isArrayWithNestedArrays} from '@/utils/hasNestedArray';
+import wait from '@/utils/wait';
+
+const useRefreshing = () => {
+  const invalidateQueries = useInvalidateQueries();
+  const [isRefresh, setIsRefresh] = useState(false);
+
+  const onRefresh = useCallback(
+    async (queryKeys: QueryKey[] | QueryKey) => {
+      if (!queryKeys) return;
+      setIsRefresh(true);
+      try {
+        await wait(200);
+        if (isArrayWithNestedArrays(queryKeys as QueryKey[])) {
+          invalidateQueries.multiple(queryKeys as QueryKey[]);
+        } else {
+          invalidateQueries.single(queryKeys);
+        }
+      } catch (err) {
+        console.log('err', err);
+      } finally {
+        setIsRefresh(false);
+      }
+    },
+    [invalidateQueries],
+  );
+
+  return useMemo(() => ({onRefresh, isRefresh}), [onRefresh, isRefresh]);
+};
+
+export default useRefreshing;
