83b5a5c2aa7454a123c24128c4292f3fb0ecf19f
suminhan123 <suminh324@gmail.com>
2024-02-06 07:30:27 +0900

    feat: 툴팁 컴포넌트 side, color prop 추가

--- src/components/tooltip/Tooltip.style.ts
+++ src/components/tooltip/Tooltip.style.ts
@@ -1,10 +1,31 @@
 import palette from '@/styles/theme/color';
+import {KeyofText} from '@/types/theme/typography';
 import {StyleSheet} from 'react-native';
 
+interface ITypeTooltip {
+  textColor: string;
+  backgroundColor: string;
+  font: KeyofText;
+}
+
+export const tooltipTypeStyles: Record<'secondary' | 'primary', ITypeTooltip> =
+  {
+    primary: {
+      textColor: palette.Primary.Dark,
+      backgroundColor: palette.Primary.Normal,
+      font: 'Label3',
+    },
+    secondary: {
+      textColor: palette.Another.White,
+      backgroundColor: 'rgba(32, 31, 30, 0.7)',
+      font: 'Body2',
+    },
+  };
 const tooltipStyles = StyleSheet.create({
   container: {
     marginBottom: 4,
-    alignItems: 'center',
+    //alignItems: 'center',
+    //alignItems: 'flex-end',
   },
   typographyWrap: {
     paddingVertical: 4,
