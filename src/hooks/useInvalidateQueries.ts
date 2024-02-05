d3d23cae69a470a160aac5ae720b399a7efbc350
suminhan123 <suminh324@gmail.com>
2024-02-05 19:09:07 +0900

    feat: 쿼리 무효화 hook 생성

---
+++ src/hooks/useInvalidateQueries.ts
@@ -0,0 +1,22 @@
+import {QueryKey, useQueryClient} from '@tanstack/react-query';
+
+const useInvalidateQueries = () => {
+  const queryClient = useQueryClient();
+
+  const invalidateSingleQuery = (queryKey: QueryKey) => {
+    queryClient.invalidateQueries({queryKey: [queryKey]});
+  };
+
+  const invalidateMultipleQueries = (queryKeys: QueryKey[]) => {
+    queryKeys.forEach(queryKey => {
+      queryClient.invalidateQueries({queryKey: [queryKey]});
+    });
+  };
+
+  return {
+    single: invalidateSingleQuery,
+    multiple: invalidateMultipleQueries,
+  };
+};
+
+export default useInvalidateQueries;
