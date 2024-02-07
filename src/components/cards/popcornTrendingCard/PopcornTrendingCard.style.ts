224eff719e36cd33b5b573208b4155b787ffafc4
gs0428 <lider28@naver.com>
2024-02-07 15:01:31 +0900

    refactor: 이번 주 팝콘작 제목 떠있는 style 해결

--- src/components/cards/popcornTrendingCard/PopcornTrendingCard.style.ts
+++ src/components/cards/popcornTrendingCard/PopcornTrendingCard.style.ts
@@ -2,28 +2,28 @@ import {StyleSheet} from 'react-native';
 
 const commonImageProps = {
   borderRadius: 16,
-  marginBottom: 4,
+  marginLeft: 16,
 };
 
 export const popcornContainerStyles = StyleSheet.create({
   'without-ranking': {
+    marginLeft: commonImageProps.marginLeft,
     width: 108,
-    marginLeft: 16,
   },
   'with-ranking': {
+    marginLeft: commonImageProps.marginLeft,
     width: 92,
-    marginLeft: 16,
   },
 });
 
 export const popcornImageStyles = StyleSheet.create({
   'without-ranking': {
-    ...commonImageProps,
+    borderRadius: commonImageProps.borderRadius,
     width: 108,
     height: 160,
   },
   'with-ranking': {
-    ...commonImageProps,
+    borderRadius: commonImageProps.borderRadius,
     width: 92,
     height: 136,
   },
