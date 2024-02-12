1bd2cfd92d7b50053f10e141cb64854ec5a48716
gs0428 <lider28@naver.com>
2024-02-12 14:43:41 +0900

    refactor: 팝콘파티 홈 스켈레톤 추가

--- src/screens/popCornParty/home/components/voteNextPopcorn/index.tsx
+++ src/screens/popCornParty/home/components/voteNextPopcorn/index.tsx
@@ -1,4 +1,5 @@
 import DefaultContainer from '@/components/container/defaultContainer';
+import EmptyItem from '@/components/items/emptyItem';
 import PopcornItem from '@/components/items/popcornItem';
 import Loading from '@/components/loading';
 import SubTitle from '@/components/title/subTitle';
@@ -25,21 +26,30 @@ const VoteNextPopcorn = ({
       {isLoading ? (
         <Loading />
       ) : (
-        <DefaultContainer>
-          {popcornRecommendData &&
-            popcornRecommendData.map((popcornData: TPopcornRecommendData) => (
-              <PopcornItem
-                key={popcornData.movieTitle}
-                id={popcornData.id}
-                movieTitle={popcornData.movieTitle}
-                imageUrl={popcornData.imageUrl}
-                recommendationCount={popcornData.recommendationCount}
-                recommendationReason={popcornData.recommendationReason}
-                movieDirector={popcornData.movieDirector}
-                voteMovieMutate={voteMovieMutate}
-              />
-            ))}
-        </DefaultContainer>
+        <>
+          {popcornRecommendData.length === 0 && (
+            <EmptyItem text="아직 추천 목록이 없어요" />
+          )}
+          {popcornRecommendData.length > 0 && (
+            <DefaultContainer>
+              {popcornRecommendData &&
+                popcornRecommendData.map(
+                  (popcornData: TPopcornRecommendData) => (
+                    <PopcornItem
+                      key={popcornData.movieTitle}
+                      id={popcornData.id}
+                      movieTitle={popcornData.movieTitle}
+                      imageUrl={popcornData.imageUrl}
+                      recommendationCount={popcornData.recommendationCount}
+                      recommendationReason={popcornData.recommendationReason}
+                      movieDirector={popcornData.movieDirector}
+                      voteMovieMutate={voteMovieMutate}
+                    />
+                  ),
+                )}
+            </DefaultContainer>
+          )}
+        </>
       )}
     </>
   );
