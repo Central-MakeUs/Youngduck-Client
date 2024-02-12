909ca2c6112959ee8be3143ef16d2970f5d66e14
suminhan123 <suminh324@gmail.com>
2024-02-13 00:53:06 +0900

    feat: 공백 여부 체크 util 함수 생성

--- src/utils/checkValue.ts
+++ src/utils/checkValue.ts
@@ -6,8 +6,21 @@ const removeWhitespace = (text: string) => {
   return text.replace(regex, '');
 };
 
+// 공백이 있는 지 체크하는 함수
+export const notContainsWhiteSpace = (str: string) => {
+  const pattern = /\s/;
+  const matches = str.match(pattern);
+  if (matches && matches.length > 0) {
+    return false;
+  }
+  return true;
+};
+
 // 이메일 체크하는 함수
 const checkEmail = (email: string) => {
+  if (email === '' || !email) {
+    return true;
+  }
   const regex =
     /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
   return regex.test(email);
